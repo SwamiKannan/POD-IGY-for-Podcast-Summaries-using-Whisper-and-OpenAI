e916a7d40b5e36ea79c40c384a312dfd1a001f31
Swaminathan Kannan <65940566+SwamiKannan@users.noreply.github.com>
2023-09-01 18:54:14 +0530

    Update podcast_backend.py

--- podcast_backend.py
+++ podcast_backend.py
@@ -14,9 +14,13 @@ stub = modal.Stub("corise-podcast-project")
 corise_image = modal.Image.debian_slim().pip_install("feedparser",
                                                      "https://github.com/openai/whisper/archive/9f70a352f9f8630ab3aa0d06af5cb9532bd8c21d.tar.gz",
                                                      "requests",
-                                                     "ffmpeg").apt_install("ffmpeg").run_function(download_whisper)
+                                                     "ffmpeg",
+                                                     "openai",
+                                                     "tiktoken",
+                                                     "wikipedia",
+                                                     "ffmpeg-python").apt_install("ffmpeg").run_function(download_whisper)
 
-@stub.function(image=corise_image, gpu="any")
+@stub.function(image=corise_image, gpu="any", timeout=600)
 def get_transcribe_podcast(rss_url, local_path):
   print ("Starting Podcast Transcription Function")
   print ("Feed URL: ", rss_url)
@@ -25,6 +29,9 @@ def get_transcribe_podcast(rss_url, local_path):
   # Read from the RSS Feed URL
   import feedparser
   intelligence_feed = feedparser.parse(rss_url)
+  podcast_title = intelligence_feed['feed']['title']
+  episode_title = intelligence_feed.entries[0]['title']
+  episode_image = intelligence_feed['feed']['image'].href
   for item in intelligence_feed.entries[0].links:
     if (item['type'] == 'audio/mpeg'):
       episode_url = item.href
@@ -61,9 +68,121 @@ def get_transcribe_podcast(rss_url, local_path):
 
   # Return the transcribed text
   print ("Podcast transcription completed, returning results...")
-  return result
+  output = {}
+  output['podcast_title'] = podcast_title
+  output['episode_title'] = episode_title
+  output['episode_image'] = episode_image
+  output['episode_transcript'] = result['text']
+  return output
+
+@stub.function(image=corise_image, secret=modal.Secret.from_name("my-openai-secret"))
+def get_podcast_summary(podcast_transcript):
+  import openai
+  instructPrompt = """
+  Summarize the following text on insights from the corporate world. The summary should have a maximum limit of 250 words.
+  """
+
+  request = instructPrompt + podcast_transcript
+  chatOutput = openai.ChatCompletion.create(model="gpt-3.5-turbo-16k",
+                                              messages=[{"role": "system", "content": "You are a helpful assistant."},
+                                                        {"role": "user", "content": request}
+                                                        ]
+                                              )
+  podcastSummary = chatOutput.choices[0].message.content
+  return podcastSummary
+
+@stub.function(image=corise_image, secret=modal.Secret.from_name("my-openai-secret"))
+def get_podcast_guest(podcast_transcript):
+  import openai
+  import wikipedia
+  import json
+  request = podcast_transcript[:10000]
+  completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": request}],
+    functions=[
+    {
+        "name": "get_podcast_guest_information",
+        "description": "Get information on the podcast host and their guest (if any) using their name to search on Wikipedia",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "guest_name": {
+                    "type": "string",
+                    "description": "The names of the host and the guest (if any)",
+                },
+                "unit": {"type": "string"},
+            },
+            "required": ["guest_name"],
+        },
+    }
+    ],
+    function_call={"name": "get_podcast_guest_information"}
+    )
+  podcastGuest={}
+  podcast_guest = ""
+  response_message = completion["choices"][0]["message"]
+  if response_message.get("function_call"):
+    function_name = response_message["function_call"]["name"]
+
+    function_args = json.loads(response_message["function_call"]["arguments"])
+    podcast_guest=function_args.get("guest_name")
+  podcastGuest['name']=function_args.get("guest_name")
+  try:
+    input = wikipedia.page(podcast_guest, auto_suggest=False)
+    podcastGuest['summary']=input.summary= input.summary
+  except:
+      try:
+        input = wikipedia.page(podcast_guest + " " + podcast_guest_org + " " + podcast_guest_title, auto_suggest=True)
+        podcastGuest['summary']=input.summary
+      except:
+        print('Guest not found on Wikipedia')
+        podcastGuest['summary']="No information available about the host"
+  return podcastGuest
+
+@stub.function(image=corise_image, secret=modal.Secret.from_name("my-openai-secret"))
+def get_podcast_highlights(podcast_transcript):
+  import openai
+  instructPrompt = """
+  You are an expert in extracting the insights from a text. You are responsible for reviewing podcast transcripts to highlight the key insights from the podcast. Identify the five most significant insights in the podcast.
+  - Each insight should be a statement by the podcast host.
+  - Each insight must be impactful.
+  - Each highlight must be concise.
+  - The insight should make the listener want to listen to the entire podcast.
+
+  Present the insights as a bulleted list which includes the full sentence of the highlight and nothing else.The output starts as follows:
+  Key Insights:
+  """
+
+
+
+  request = instructPrompt + podcast_transcript
+
+  chatOutput = openai.ChatCompletion.create(model="gpt-3.5-turbo-16k",
+                                              messages=[{"role": "system", "content": "You are a helpful assistant."},
+                                                        {"role": "user", "content": request}
+                                                        ]
+                                              )
+  podcastHighlights = chatOutput.choices[0].message.content
+  return podcastHighlights
+
+@stub.function(image=corise_image, secret=modal.Secret.from_name("my-openai-secret"), timeout=1200)
+def process_podcast(url, path):
+  output = {}
+  podcast_details = get_transcribe_podcast.call(url, path)
+  podcast_summary = get_podcast_summary.call(podcast_details['episode_transcript'])
+  podcast_guest = get_podcast_guest.call(podcast_details['episode_transcript'])
+  podcast_highlights = get_podcast_highlights.call(podcast_details['episode_transcript'])
+  output['podcast_details'] = podcast_details
+  output['podcast_summary'] = podcast_summary
+  output['podcast_guest'] = podcast_guest
+  output['podcast_highlights'] = podcast_highlights
+  return output
 
 @stub.local_entrypoint()
-def main(url, path):
-  output = get_transcribe_podcast.call(url, path)
-  print (output['text'])
+def test_method(url, path):
+  output = {}
+  podcast_details = get_transcribe_podcast.call(url, path)
+  print ("Podcast Summary: ", get_podcast_summary.call(podcast_details['episode_transcript']))
+  print ("Podcast Guest Information: ", get_podcast_guest.call(podcast_details['episode_transcript']))
+  print ("Podcast Highlights: ", get_podcast_highlights.call(podcast_details['episode_transcript']))
